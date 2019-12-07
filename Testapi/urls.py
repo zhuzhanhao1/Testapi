401ba53e6a9bca8f96be37946806c64be0c73474
971567069@qq.com <zzh1996929@163.com>
2019-12-07 15:08:53 +0800

    调整结构

--- Testapi/urls.py
+++ Testapi/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('api.urls')),
+    url(r'^', include('Api.api.urls')),
     # url(r'^',include('Web.urls')),
 ]
